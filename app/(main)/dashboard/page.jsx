845e1ef144ca46d85d78e54b1ac846fca6361e56
saumyasingh003 <saumyasingh98982@gmail.com>
2025-03-16 22:35:38 +0530

    updated

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,19 +1,19 @@
-"use client";
+
 import { getIndustryInsights } from "@/actions/dashboard";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
 import DashboardView from "./_components/DashboardView";
-import { useEffect } from "react";
+
 
 const industryInsightPage = async () => {
   const { isOnboarded } = await getUserOnboardingStatus();
   const insights = await getIndustryInsights();
-useEffect(()=>{
 
-  if (!isOnboarded) {
+
+  if (!isOnboarded && !insights) {
     redirect("/onboarding");
   }
-},[])
+
   return (
     <div className="container mx-auto">
       <DashboardView insights={insights} />
